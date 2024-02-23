16a90747b0d38d6bf129f538df17ee8fb9e12df0
Maruf Siddiki Galib <145563201+MarufSiddikiGalib@users.noreply.github.com>
2024-02-24 04:34:21 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './App.css';
-
+import {AppContext} from "./context/contextApi";
 function App() {
   return (
+    <AppContext>
     <div className="text-5xl text-center text-red-500">App</div>
-    
+    </AppContext>
   );
 }
 
