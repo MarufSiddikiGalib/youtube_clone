93c0db6000c26f1dd1f6a2746a69fa81c7143d17
Maruf Siddiki Galib <145563201+MarufSiddikiGalib@users.noreply.github.com>
2024-02-26 01:01:57 +0600

    Added Component Files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import './App.css';
 import {AppContext} from "./context/contextApi";
+import Header from "./Components/Header";
+import Feed from "./Components/Feed";
+import LeftNav from "./Components/LeftNav";
+import LeftNavMenu from "./Components/LeftNavMenu";
+import SearchResult from "./Components/SearchResult";
+import SearchResultVideoCard from "./Components/SearchResultVideoCard";
+import SuggestionVideoCard from "./Components/SuggestionVideoCard";
+import VideoCard from "./Components/VideoCard";
+import VideoDetails from "./Components/VideoDetails";
+
 function App() {
   return (
     <AppContext>
