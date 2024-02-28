a89f52cc2bf92ffb1c5a4fc22106a48387b73bea
Maruf Siddiki Galib <145563201+MarufSiddikiGalib@users.noreply.github.com>
2024-02-28 13:52:41 +0600

    Updated LeftNavMenu

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
+import { Context } from '../context/contextApi'
+import LeftNav from './LeftNav'
+
+
 
 const Feed = () => {
   return (
-    <div>
-      
+    <div className="flex flex-row h-[calc(100%-56px)]">
+
+        <LeftNav />
+
     </div>
-  )
+  );
+  
 }
 
 export default Feed
--- src/Components/LeftNav.jsx
+++ src/Components/LeftNav.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { useNavigate } from "react-router-dom";
+import { categories } from "../utils/constants";
+import { Context } from "../context/contextApi";
+import LeftNavMenu from './LeftNavMenu';
 
 const LeftNav = () => {
+
+  const { selectCategories, setSelectCategories, mobileMenu } =
+  useContext(Context);
+
+const navigate = useNavigate();
+
+const clickHandler = (name, type) => {
+  switch (type) {
+      case "category":
+          return setSelectCategories(name);
+      case "home":
+          return setSelectCategories(name);
+      case "menu":
+          return false;
+      default:
+          break;
+  }
+};
+
   return (
-    <div>
-      
+    <div className='md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all'
+    >
+
+      <div className="flex px-5 flex-col">
+       {categories.map((item)=>{
+          return (
+            <React.Fragment> {/*  gives error without react fragment we can't use sibling div without parent div or react fragment  */}
+
+                <LeftNavMenu
+                      text = {item.type ==="home" ? "Home" : item.name}
+                      icon ={item.icon}
+                      action={() => {
+                        clickHandler(item.name, item.type)
+                        navigate("/")
+                      }}
+                      className = {`${selectCategories === item.name ? "bg white/[0.15]" : "" }`}
+                      /* for hightlight items */
+                       
+          
+                />
+
+                {item.divider && (
+                    <hr className='my-5 border-white/[0.2]'/>
+                )}
+    
+            </React.Fragment>
+          )
+       })}
+       <hr className='my-5 border-white/[0.2]'/>
+       <div className='text-white/[0.5] text-[12px]'>  Developed by @marufsiddikigalib
+      </div>
+     </div>
     </div>
+    
   )
 }
 
