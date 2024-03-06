613c5a4eb461aac35268030533e097e3949323cd
Maruf Siddiki Galib <145563201+MarufSiddikiGalib@users.noreply.github.com>
2024-03-06 19:58:02 +0600

    Updated Feed

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -7,10 +7,22 @@ import LeftNav from './LeftNav'
 const Feed = () => {
   return (
     <div className="flex flex-row h-[calc(100%-56px)]">
-
-        <LeftNav />
-
+    <LeftNav />
+    <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+            {/* {!loading &&
+                searchResults.map((item) => {
+                    if (item.type !== "video") return false;
+                    return (
+                        <VideoCard
+                            key={item?.video?.videoId}
+                            video={item?.video}
+                        />
+                    );
+                })} */}
+        </div>
     </div>
+</div>
   );
   
 }
